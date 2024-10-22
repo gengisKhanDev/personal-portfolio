b0808ddc833b7c71312d3e721b304021c8300d2c
harakiriOP <salvadorhidalgo100@gmail.com>
2024-10-22 18:12:10 -0500

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js', // La entrada de tu proyecto
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/', // Esto asegura que los archivos sean servidos desde la raíz
+    publicPath: '/', // Para que las rutas funcionen correctamente
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/, // Procesa imágenes y fuentes
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html', // Asegúrate de que este es tu archivo HTML
+      template: './public/index.html', // Usa tu archivo HTML de la carpeta public
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: 'dist' }, // Copia todos los archivos de 'public' a 'dist'
+      ],
     }),
   ],
   devServer: {
